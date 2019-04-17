364dd3e921097b20291b9f6c771abdf7635a02c7
Ryan Kubik <ryan.kubik@nike.com>
2019-04-17 10:37:59 -0700

    Test that an initial start location that fails will not return itself and test that a split matrix only returns connected

--- pathfinding/floodFill.test.js
+++ pathfinding/floodFill.test.js
@@ -1,17 +1,15 @@
-import { pipe, flatten } from "ramda";
+import { pipe, flatten, identity, always, __ as gap } from "ramda";
 import floodFill from "./floodFill";
 import getNeighbors from "../matricies/getNeighbors";
-import { initMatrix, mapMatrix } from "../matricies";
+import { initMatrix, mapMatrix, fillMatrix, updateMatrix } from "../matricies";
 import expectToEqualArray from "../testUtils/expectToEqualArray";
 import { CROSS_NEIGHBORS } from "../matricies/directions";
 
 describe("floodFill", () => {
   it("should return empty array if open is empty", () => {
-    const checkLocation = () => true;
-
     const filled = floodFill(
       getNeighbors(CROSS_NEIGHBORS),
-      checkLocation,
+      always(true),
       initMatrix({ width: 10, height: 10 }),
       [],
       [],
@@ -22,12 +20,11 @@ describe("floodFill", () => {
   });
 
   it("should return entire matrix of locations", () => {
-    const checkLocation = () => true;
     const dimensions = { width: 3, height: 3 };
 
     const filled = floodFill(
       getNeighbors(CROSS_NEIGHBORS),
-      checkLocation,
+      always(true),
       initMatrix(dimensions),
       [{ row: 0, col: 0 }],
       [],
@@ -42,5 +39,60 @@ describe("floodFill", () => {
         flatten
       )(dimensions)
     );
+  });
+
+  it("should return empty array if start location fails checkLocation", () => {
+    const dimensions = { width: 3, height: 3 };
+
+    const filled = floodFill(
+      getNeighbors(CROSS_NEIGHBORS),
+      always(false),
+      initMatrix(dimensions),
+      [{ row: 0, col: 0 }],
+      [],
+      []
+    );
+
+    expectToEqualArray(filled, []);
+  });
+
+  it("should return empty array if start location fails checkLocation", () => {
+    const dimensions = { width: 3, height: 3 };
+
+    const filled = floodFill(
+      getNeighbors(CROSS_NEIGHBORS),
+      always(false),
+      initMatrix(dimensions),
+      [{ row: 0, col: 0 }],
+      [],
+      []
+    );
+
+    expectToEqualArray(filled, []);
+  });
+
+  it("should return only locations connected to start", () => {
+    const dimensions = { width: 3, height: 3 };
+    const splitMatrix = pipe(
+      fillMatrix(gap, true),
+      updateMatrix({ row: 0, col: 1 }, false),
+      updateMatrix({ row: 1, col: 1 }, false),
+      updateMatrix({ row: 2, col: 1 }, false)
+    )(dimensions);
+
+    const filled = floodFill(
+      getNeighbors(CROSS_NEIGHBORS),
+      identity,
+      splitMatrix,
+      [{ row: 0, col: 0 }],
+      [],
+      []
+    );
+
+    expectToEqualArray(filled, [
+      { row: 0, col: 0 },
+      { row: 1, col: 0 },
+      { row: 2, col: 0 }
+    ]);
   });
 });
