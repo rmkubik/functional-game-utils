8b0b8bb03fece70ea67db1e1d5d565e957f7582d
Ryan Kubik <ryan.kubik@nike.com>
2019-04-18 16:09:04 -0700

    Create findLocation and tests

--- matricies/find.js
+++ matricies/find.js
@@ -1,10 +1,26 @@
-import { find, curry, pipe, flatten } from "ramda";
+import { find, curry, pipe, flatten, findIndex, any } from "ramda";
+import { getRow } from "./getters";
 
-const findInMatrix = curry((comparator, matrix) =>
+const findValue = curry((comparator, matrix) =>
   pipe(
     flatten,
     find(comparator)
   )(matrix)
 );
 
-export { findInMatrix };
+const findLocation = curry((comparator, matrix) => {
+  const row = findIndex(row => any(comparator, row), matrix);
+
+  if (row === -1) {
+    return undefined;
+  }
+
+  const col = findIndex(comparator, getRow(matrix, row));
+
+  return {
+    row,
+    col
+  };
+});
+
+export { findValue, findLocation };
--- matricies/find.test.js
+++ matricies/find.test.js
@@ -1,8 +1,8 @@
 import { pipe, pathEq, clone } from "ramda";
-import { findInMatrix } from "./find";
+import { findValue, findLocation } from "./find";
 import { initMatrix, updateMatrix } from ".";
 
-describe("findInMatrix", () => {
+describe("findValue", () => {
   it("should find value in matrix if comparator matches", () => {
     const target = { node: { start: true } };
 
@@ -11,7 +11,7 @@ describe("findInMatrix", () => {
       updateMatrix({ row: 3, col: 7 }, target)
     )({ width: 10, height: 10 });
 
-    const value = findInMatrix(pathEq(["node", "start"], true), matrix);
+    const value = findValue(pathEq(["node", "start"], true), matrix);
 
     expect(value).toEqual(clone(target));
   });
@@ -24,8 +24,38 @@ describe("findInMatrix", () => {
       updateMatrix({ row: 3, col: 7 }, target)
     )({ width: 10, height: 10 });
 
-    const value = findInMatrix(pathEq(["node", "start"], true), matrix);
+    const value = findValue(pathEq(["node", "start"], true), matrix);
 
     expect(value).toBe(undefined);
+  });
+});
+
+describe("findLocation", () => {
+  it("should return found location", () => {
+    const target = { thing: { prop: true } };
+    const targetLocation = { row: 3, col: 7 };
+
+    const matrix = pipe(
+      initMatrix,
+      updateMatrix(targetLocation, target)
+    )({ width: 10, height: 10 });
+
+    const location = findLocation(pathEq(["thing", "prop"], true), matrix);
+
+    expect(location).toEqual(targetLocation);
+  });
+
+  it("should return undefined if target not in matrix", () => {
+    const target = { incorrect: { value: false } };
+    const targetLocation = { row: 3, col: 7 };
+
+    const matrix = pipe(
+      initMatrix,
+      updateMatrix(targetLocation, target)
+    )({ width: 10, height: 10 });
+
+    const location = findLocation(pathEq(["thing", "prop"], true), matrix);
+
+    expect(location).toBe(undefined);
   });
 });
