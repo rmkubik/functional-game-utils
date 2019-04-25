b6618f67380619369d9b4f19196c278dc3262a1f
Ryan Kubik <rmkubik@me.com>
2019-04-24 18:01:17 -0700

    Export all matrix functions

--- src/matrices/index.js
+++ src/matrices/index.js
@@ -3,5 +3,17 @@ import initMatrix from "./initMatrix";
 import mapMatrix from "./mapMatrix";
 import constructMatrix from "./constructMatrix";
 import updateMatrix from "./updateMatrix";
+import getNeighbors from "./getNeighbors";
 
-export { fillMatrix, initMatrix, mapMatrix, constructMatrix, updateMatrix };
+export {
+  fillMatrix,
+  initMatrix,
+  mapMatrix,
+  constructMatrix,
+  updateMatrix,
+  getNeighbors
+};
+export * from "./directions";
+export * from "./find";
+export * from "./getters";
+export * from "./locations";
