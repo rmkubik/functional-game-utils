d8827b115554790db84f656b4c05dc67eca0adb3
Ryan Kubik <rmkubik@me.com>
2019-04-29 12:45:44 -0700

    Document assert module

--- src/assert/index.js
+++ src/assert/index.js
@@ -1,9 +1,29 @@
+/**
+ * @description A special error that indicates an assertion has failed.
+ *
+ * @extends Error
+ */
 class AssertionError extends Error {
+  /**
+   * @param {string} message - The message containing more information about a given AssertionError.
+   */
   constructor(message) {
     super(`AssertionError: ${message}`);
   }
 }
 
+/**
+ * @description If the assertion is truthy then nothing will happen. Otherwise, this function throws
+ * an {@link AssertionError} with the provided message if the assertion is falsy.
+ *
+ * @example
+ * function example(param) {
+ *   assert(typeof param === 'string', 'Param must be of type string!');
+ * }
+ *
+ * @param {bool} assertion
+ * @param {string} message
+ */
 const assert = (assertion, message) => {
   if (!assertion) {
     throw new AssertionError(message);
