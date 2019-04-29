f3fdee1e68153f5e390206640117fa2e7392147e
Ryan Kubik <rmkubik@me.com>
2019-04-29 13:57:27 -0700

    Pass location into constructMtrix callback

--- src/matrices/constructMatrix.js
+++ src/matrices/constructMatrix.js
@@ -1,10 +1,25 @@
 import { curry, pipe } from "ramda";
 import { initMatrix, mapMatrix } from "./index";
 
+/**
+ * @description A function used to construct the value of a given matrix location.
+ *
+ * @callback MatrixConstructor
+ * @param {Location} location - The location of the constructed value.
+ */
+
+/**
+ * @description Creates a matrix of the specified dimensions where each location is filled with the
+ * result of running the constructor function.
+ *
+ * @param {MatrixConstructor} constructor - The function used to construct each value of the array.
+ * @param {Dimensions} dimensions - The length of the array to be filled.
+ * @returns {Object[][]} Newly created matrix of the constructed values.
+ */
 const constructMatrix = curry((constructor, dimensions) =>
   pipe(
     initMatrix,
-    mapMatrix(constructor)
+    mapMatrix((_, location) => constructor(location))
   )(dimensions)
 );
 
--- src/matrices/constructMatrix.test.js
+++ src/matrices/constructMatrix.test.js
@@ -1,4 +1,5 @@
 import constructMatrix from "./constructMatrix";
+import expectToEqualArray from "../../testUtils/expectToEqualArray";
 
 describe("constructMatrix", () => {
   it("should create a matrix with provided function and dimensions", () => {
@@ -27,5 +28,17 @@ describe("constructMatrix", () => {
     const matrix = constructMatrix(() => ++i, { width: 2, height: 3 });
 
     expect(matrix).toEqual([[1, 2], [3, 4], [5, 6]]);
+  });
+
+  it("should expose the current location as a prop to the constructor", () => {
+    const matrix = constructMatrix(location => location, {
+      width: 2,
+      height: 2
+    });
+
+    expectToEqualArray(matrix, [
+      [{ row: 0, col: 0 }, { row: 0, col: 1 }],
+      [{ row: 1, col: 0 }, { row: 1, col: 1 }]
+    ]);
   });
 });
