16f7f299b32065ac53dfd1bc206c31be9c3ea307
Ryan Kubik <rmkubik@me.com>
2020-12-19 14:24:48 -0800

    Calculate headers and doc nav scroll

--- docs/components/Header.js
+++ docs/components/Header.js
@@ -4,19 +4,21 @@ import styles from "./Header.module.scss";
 
 const Header = () => {
   return (
-    <header className={styles.header}>
-      <h1>
-        <Link href="/">Functional Game Utils</Link>
-      </h1>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/docs">Docs</Link>
-        </li>
-      </ul>
-    </header>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1>
+          <Link href="/">Functional Game Utils</Link>
+        </h1>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/docs">Docs</Link>
+          </li>
+        </ul>
+      </header>
+    </div>
   );
 };
 
