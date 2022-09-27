5f76af67ac7b371a3eb0f0bc4f328a236540c89a
Ryan Kubik <rmkubik@me.com>
2022-09-26 19:19:42 -0700

    Document fillMatrix and find functions

--- src/matrices/fillMatrix.js
+++ src/matrices/fillMatrix.js
@@ -5,6 +5,15 @@
 import { pipe, curry } from "ramda";
 import { fillArray } from "../arrays";
 
+/**
+ * @description Creates a matrix of the specified dimensions filled
+ * with the provided value at every location.
+ *
+ * @param {Dimensions} dimensions - width and height of this matrix
+ * @param {Object} value - any value to fill the matrix with
+ *
+ * @returns {Object[][]} the newly created array
+ */
 const fillMatrix = curry(({ width, height }, value) =>
   pipe(fillArray(width), fillArray(height))(value)
 );
--- src/matrices/find.js
+++ src/matrices/find.js
@@ -5,6 +5,26 @@
 import { find, curry, pipe, flatten, findIndex, any } from "ramda";
 import { maxWidth, height } from "./dimensions";
 
+/**
+ * @callback CellComparator
+ *
+ * @param {Object} cell - the cell's value at the current location
+ * @param {Location} location - the location being checked
+ *
+ * @returns {boolean} true if this cell/location satisfies the condition
+ */
+
+/**
+ * @description Search a provided matrix for the first cell that satisfies
+ * the provided comparator's condition. Returns that cell's value.
+ *
+ * @param {CellComparator} comparator - callback to check if any given cell
+ * or location should be found.
+ * @param {Object[][]} matrix - matrix to be searched
+ *
+ * @returns {(Object|undefined)} the found value or undefined if nothing
+ * matching the comparator was found.
+ */
 const findValue = curry((comparator, matrix) => {
   let found;
 
@@ -19,6 +39,17 @@ const findValue = curry((comparator, matrix) => {
   return found;
 });
 
+/**
+ * @description Search a provided matrix for the first cell that satisfies
+ * the provided comparator's condition. Return's that cell's location.
+ *
+ * @param {CellComparator} comparator - callback to check if any given cell
+ * or location should be found.
+ * @param {Object[][]} matrix - matrix to be searched
+ *
+ * @returns {(Location|undefined)} the found value or undefined if nothing
+ * matching the comparator was found.
+ */
 const findLocation = curry((comparator, matrix) => {
   let found = false;
   let location = {};
@@ -35,6 +66,17 @@ const findLocation = curry((comparator, matrix) => {
   return found ? location : undefined;
 });
 
+/**
+ * @description Search a provided matrix for the all cell that satisfy
+ * the provided comparator's condition. Returns all matching locations.
+ *
+ * @param {CellComparator} comparator - callback to check if any given cell
+ * or location should be found.
+ * @param {Object[][]} matrix - matrix to be searched
+ *
+ * @returns {Array<Location>} array containing all matching locations in the
+ * provided matrix.
+ */
 const findLocations = curry((comparator, matrix) => {
   const matches = [];
 
